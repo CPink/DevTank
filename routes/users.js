7d6ac041f69ce3c484e6c10e182809ef1580790c
Chance Pinkerton <C_pink@Chances-MacBook-Pro.local>
2018-02-01 11:53:55 -0700

    added passport via npm, added _errors partial view, added _errors to main.handlebars, and added the values for the register form with some clean up

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 const router = express.Router();
 
 module.exports = router;
@@ -14,4 +17,29 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
     //res.send('register');
     res.render('users/register');
+});
+
+//register form POST
+router.post('/register', (req, res) => {
+    let errors = [];
+
+    if(req.body.password != req.body.password2){
+        errors.push({text: 'Passwords must match'})
+    }
+
+    if(req.body.password.length < 4){
+        errors.push({text: 'Password must be atleast 4 characters'})
+    }
+
+    if(errors.length > 0){
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
+        });       
+    }else{
+        res.send('passed')
+    }
 });
